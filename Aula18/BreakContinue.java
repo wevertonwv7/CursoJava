616cdb59df2d742faa24ac6170ac7a3d88ef3690
weviton_silva <Jacareblindado@9738>
2024-01-02 21:47:32 -0300

    aula 18

--- Aula18/BreakContinue.java
+++ Aula18/BreakContinue.java
@@ -1,7 +1,41 @@
 package Aula18;
 
+import java.util.Scanner;
+
 public class BreakContinue {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in); 
+
+          /* 
+          System.out.println("Entre com um númnero inicial");
+        int num = scan.nextInt(); 
+
+        System.out.println("Entre com um número limite");
+        int max = scan.nextInt();
+
+        for(int i = num; i<= max; i++){
+
+            if(i % 7 == 0){
+                System.out.println("O Valor de I é "+ i);
+                break;
+            }
+        }*/
+
+
+        System.out.println("Entre com um númnero inicial");
+        int num = scan.nextInt(); 
+
+        System.out.println("Entre com um número limite");
+        int max = scan.nextInt();
+
+        for(int i = num; i<= max; i++){
+
+            if(i % 7 == 0){
+                continue;
+            }
+            System.out.println("O Valor de i é "+ i);
+        }
         
     }
     
